1860edce424b330781f7189ca40911658257540d
namcorp1 <nikitamoskalev95@gmail.com>
2022-11-27 16:31:06 +0700

    Add method for input data

---
+++ solution_CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Написать программу, 
+которая из имеющегося массива строк 
+формирует массив из строк, 
+длина которых меньше либо равна 3 символа. */
+
+// ввод данных
+string[] InputData()
+{
+    string[] input = new string[1];
+    string check = string.Empty;
+    int index = 0;
+    while (check != "exit input")
+    {
+        Console.Write("Введите строку: ");
+        check = Console.ReadLine();
+        if (check == "exit input") break;
+        else
+        {
+            input[index] = check;
+            Array.Resize(ref input, input.Length + 1);
+            index++;
+        }
+    }
+    Array.Resize(ref input, input.Length - 1);
+    return input;
+}
